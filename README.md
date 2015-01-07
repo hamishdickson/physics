99cff0f57695cd2bc02f94201dbf4328058f91fa
mishyTheBear <hamish.dickson@gmail.com>
2015-01-07 17:25:27 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+A redo of some simple physics stuff I did at uni
+
+except
+1) we used c++ at uni (not java), where you can do cool (read dangerous) things like overloading operators
+2) this has been done with TDD
--- src/main/java/physics/specialRelativity/SpecialRelativity.java
+++ src/main/java/physics/specialRelativity/SpecialRelativity.java
@@ -7,16 +7,6 @@ package physics.specialRelativity;
  */
 public class SpecialRelativity {
     public static void main(String[] args) {
-        ThreeVector v0 = new ThreeVector();
-        System.out.println("v0 = " + v0.toString());
-
-        ThreeVector v1 = new ThreeVector(-1.3054, 7.4033, 2.7362);
-        System.out.println("v1 = " + v1.toString());
-
-        ThreeVector v2 = new ThreeVector(8.0, 70.0, 100.0, 'd');
-        System.out.println("v2 = " + v2.toString());
-
-        ThreeVector v3 = new ThreeVector(8.0, 1.2217, 1.7453, 'r');
-        System.out.println("v3 = " + v3.toString());
+        System.out.println("nothing too special yet - see unit tests");
     }
 }
