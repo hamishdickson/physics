28081291d3777e96b5aff96b6df97b951ade2eab
mishyTheBear <hamish.dickson@gmail.com>
2015-01-10 16:00:38 +0000

    move simulation class into a test

--- src/test/java/physics/particle/ParticleTest.java
+++ src/test/java/physics/particle/ParticleTest.java
@@ -1,8 +1,21 @@
 package physics.particle;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class ParticleTest extends TestCase {
+    private static final double PION_REST_MASS = 139.6;
+    private static final double ELECTRON_REST_MASS = 0.511;
+    private static final double MUON_REST_MASS = 105.7;
+    private static final double NEUTRINO_REST_MASS = 0.0;
 
+    /**
+     * pion^{+} -> e^{+} v
+     * pion^{+} -> muon^{+} v
+     */
+    @Test
+    public void testRestDecayOfPion() {
+        
+    }
 
 }
